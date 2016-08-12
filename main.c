5fc02d0d4357769007adf5b76cb7e8cabd0efce3
spvgtx <andreymag96@gmail.com>
2016-08-12 15:09:18 +0500

    Wrote basic main

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
+#include <stdio.h>
+
 #include "chip8.h"
 
-int main(int argc, char const *argv[]) {
-                
+int main(int argc, char *argv[]) {
+        if (argc < 2) {
+                printf("Usage: chip8emu <game_to_run>\n");
+                return 0;
+        }
+        chip8_start(argv[1]);
+
         return 0;
 }
